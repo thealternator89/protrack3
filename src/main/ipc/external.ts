22ed3786aa9e33872d44e28c241b086f33cf06c8
Mark Benson <41453195+thealternator89@users.noreply.github.com>
2026-04-15 21:46:44 +1200

    feat: display app version in a footer

--- src/main/ipc/external.ts
+++ src/main/ipc/external.ts
@@ -1,7 +1,11 @@
-import { ipcMain, shell } from 'electron';
+import { ipcMain, shell, app } from 'electron';
 
 export function registerExternalHandlers() {
   ipcMain.handle('open-external', async (event, url) => {
     return await shell.openExternal(url);
+  });
+
+  ipcMain.handle('get-app-version', () => {
+    return app.getVersion();
   });
 }
